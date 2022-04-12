2c2c97d2871cecea73d8b1f76f4d186347913ff2
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-04-12 00:57:41 -0300

    agregada barra de menu, footer y hero

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="mt-16 text-3xl font-bold underline decoration-sky-500 hover:decoration-blue-600 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
-          Aquí construiremos una tienda con React.JS y TailwindCSS =)
-        </h1>
-      </header>
+      <NavBar/>
+      <main className="App-main">
+        <Hero/>
+      <Footer/>
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
