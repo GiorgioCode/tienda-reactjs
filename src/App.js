b56bc0e46c0a66f10f1bf631a792a2e4a37ef828
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-04-16 15:11:46 -0300

    actualizacion props ItemListContainer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <NavBar/>
       <main className="App-main">
         <Hero saludoinicial="¡Te estabamos esperando!" saludosecundario="¡Bienvenid@ a nuestra pizzeria online! te invitamos a hacer tu pedido presionando el siguiente botón."/>
-        <ItemListContainer/>
+        <ItemListContainer novedad1="Nueva pizza vegetariana" novedad2="Nueva Extra pepperoni" novedad3="Nueva Pepperoni sin gluten" novedad4="Al taglio extra oliva" novedad5="Napolitana tradicional" novedad6="Nueva Pizza Keto" novedad7="Extra Cuatro quesos" />
         <Footer/>
       </main>
     </div>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,25 +2,81 @@ const ItemListContainer = (props) => {
 	return (
 <div class="carousel rounded-box bg-emerald-900 p-2">
   <div class="carousel-item p-2">
-    <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=8B7BCDC2" alt="pizza" />
+        <div class="bg-white rounded-lg border shadow-md max-w-xs overflow-hidden">
+            <img class="h-56 lg:h-60 w-full object-cover" src="https://api.lorem.space/image/pizza?w=400&h=300&hash=8B7BCDC2" alt="" />
+            <div class="p-3">
+                <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
+                {props.novedad1}
+                </h3>
+                <button className="btn bg-emerald-900 btn-block text-lg text-orange-100 hover:text-white hover:bg-red-800">Añadir al carrito</button>
+            </div>
+        </div>
   </div> 
   <div class="carousel-item p-2">
-    <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=500B67FB" alt="pizza" />
+    <div class="bg-white rounded-lg border shadow-md max-w-xs overflow-hidden">
+              <img class="h-56 lg:h-60 w-full object-cover" src="https://api.lorem.space/image/pizza?w=400&h=300&hash=500B67FB" alt="" />
+              <div class="p-3">
+                  <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
+                  {props.novedad2}
+                  </h3>
+                  <button className="btn bg-emerald-900 btn-block text-lg text-orange-100 hover:text-white hover:bg-red-800">Añadir al carrito</button>
+              </div>
+          </div>
   </div> 
   <div class="carousel-item p-2">
-    <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=A89D0DE6" alt="pizza" />
+    <div class="bg-white rounded-lg border shadow-md max-w-xs overflow-hidden">
+              <img class="h-56 lg:h-60 w-full object-cover" src="https://api.lorem.space/image/pizza?w=400&h=300&hash=A89D0DE6" alt="" />
+              <div class="p-3">
+                  <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
+                  {props.novedad3}
+                  </h3>
+                  <button className="btn bg-emerald-900 btn-block text-lg text-orange-100 hover:text-white hover:bg-red-800">Añadir al carrito</button>
+              </div>
+          </div>
   </div> 
   <div class="carousel-item p-2">
-    <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=225E6693" alt="pizza" />
+    <div class="bg-white rounded-lg border shadow-md max-w-xs overflow-hidden">
+              <img class="h-56 lg:h-60 w-full object-cover" src="https://api.lorem.space/image/pizza?w=400&h=300&hash=225E6693" alt="" />
+              <div class="p-3">
+                  <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
+                  {props.novedad4}
+                  </h3>
+                  <button className="btn bg-emerald-900 btn-block text-lg text-orange-100 hover:text-white hover:bg-red-800">Añadir al carrito</button>
+              </div>
+          </div>
   </div> 
   <div class="carousel-item p-2">
-    <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=9D9539E7" alt="pizza" />
+  <div class="bg-white rounded-lg border shadow-md max-w-xs overflow-hidden">
+              <img class="h-56 lg:h-60 w-full object-cover" src="https://api.lorem.space/image/pizza?w=400&h=300&hash=9D9539E7" alt="" />
+              <div class="p-3">
+                  <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
+                  {props.novedad5}
+                  </h3>
+                  <button className="btn bg-emerald-900 btn-block text-lg text-orange-100 hover:text-white hover:bg-red-800">Añadir al carrito</button>
+              </div>
+          </div>
   </div> 
   <div class="carousel-item p-2">
-    <img src="https://api.lorem.space/image/pizza?w=400&h=300&hash=BDC01094" alt="pizza" />
+  <div class="bg-white rounded-lg border shadow-md max-w-xs overflow-hidden">
+              <img class="h-56 lg:h-60 w-full object-cover" src="https://api.lorem.space/image/pizza?w=400&h=300&hash=B0E33EF4" alt="" />
+              <div class="p-3">
+                  <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
+                  {props.novedad6}
+                  </h3>
+                  <button className="btn bg-emerald-900 btn-block text-lg text-orange-100 hover:text-white hover:bg-red-800">Añadir al carrito</button>
+              </div>
+          </div>
   </div> 
   <div class="carousel-item p-2">
-    <img src="https://api.lorem.space/image/burger?w=400&h=300&hash=7F5AE56A" alt="Burger" />
+  <div class="bg-white rounded-lg border shadow-md max-w-xs overflow-hidden">
+              <img class="h-56 lg:h-60 w-full object-cover" src="https://api.lorem.space/image/pizza?w=400&h=300&hash=7F5AE56A" alt="" />
+              <div class="p-3">
+                  <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
+                  {props.novedad7}
+                  </h3>
+                  <button className="btn bg-emerald-900 btn-block text-lg text-orange-100 hover:text-white hover:bg-red-800">Añadir al carrito</button>
+              </div>
+          </div>
   </div>
 </div>
 	);
